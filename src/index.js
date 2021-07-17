473924155cc86c4fda3c7c1d62f157beae34f6f6
picolloo <lucaspicolloo@gmail.com>
2021-07-17 09:23:20 -0300

    get column types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const { element2selector } = require('puppeteer-element2selector');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -8,14 +9,27 @@ const puppeteer = require('puppeteer');
 
   await page.goto('https://www.kaggle.com/search?q=linear+regression+in%3Adatasets')
 
-  await page.waitForSelector('div.searchTarget:nth-child(1)')
-  await page.click('div.searchTarget:nth-child(1)')
+  await page.waitForSelector('div.searchTarget')
 
+  const results = await page.$$('div.searchTarget')
 
-  // columns types
-  await page.waitForXPath('//*[@id="site-content"]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div[6]/div/div')
-  const columns = await page.$x('//*[@id="site-content"]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div[6]/div/div')
+  await results[8].click()
 
+  await page.waitForXPath('/html/body/main/div[1]/div/div[5]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div[6]/div/div')
+  const columns = await page.$x('/html/body/main/div[1]/div/div[5]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div[6]/div/div')
+
+  const columnTypes = []
+  for (let col of columns) {
+    const colSelector = await element2selector(col)
+
+    const colDiscrete = !!(await page.$(colSelector + ' span'))
+
+    if (colDiscrete) {
+      const columnValues = (await page.$$(`${colSelector} > div`)).length
+      columnTypes.push(columnValues > 2 ? "discrete" : "binary")
+    }
+    else columnTypes.push("continuous")
+  }
 
   // ncolumns
   await page.waitForXPath('//*[@id="site-content"]/div[3]/div[2]/div[3]/div[2]/div/div[2]/div/div[2]/div/div[2]/div/p')
@@ -31,17 +45,11 @@ const puppeteer = require('puppeteer');
   const nrows = await page.evaluate(name => name.innerText, nrowsEl)
 
 
-  // await page.waitForSelector('#numeric-chart-A9SwWhOc837ASvDS35vd\/versions\/6cTY6g1shQFXgt6Bpwwj\/files\/test\.csv\/columns\/hIQuwOLiZ5EqV8Z8mH4g > div > div > div > svg > g:nth-child(3)')
-  // await page.click('#numeric-chart-A9SwWhOc837ASvDS35vd\/versions\/6cTY6g1shQFXgt6Bpwwj\/files\/test\.csv\/columns\/hIQuwOLiZ5EqV8Z8mH4g > div > div > div > svg > g:nth-child(3)')
-
-  // await page.waitForSelector('.sc-jLZncB > .sc-oTqIG > .sc-pbOaj:nth-child(3) > .sc-psFCR > .sc-fznxKY')
-  // await page.click('.sc-jLZncB > .sc-oTqIG > .sc-pbOaj:nth-child(3) > .sc-psFCR > .sc-fznxKY')
-
-  // await page.waitForSelector('.sc-jxBcmK:nth-child(1) > .sc-jCDcrb > div > .sc-kbKRms > .sc-khBxaG:nth-child(2) > .sc-kIpGSl')
-  // await page.click('.sc-jxBcmK:nth-child(1) > .sc-jCDcrb > div > .sc-kbKRms > .sc-khBxaG:nth-child(2) > .sc-kIpGSl')
-
-  // await page.waitForSelector('.sc-jxBcmK:nth-child(1) > .sc-jCDcrb > div > .sc-kbKRms > .sc-khBxaG:nth-child(2) > .sc-kIpGSl')
-  // await page.click('.sc-jxBcmK:nth-child(1) > .sc-jCDcrb > div > .sc-kbKRms > .sc-khBxaG:nth-child(2) > .sc-kIpGSl')
+  console.log({
+    nrows, ncols, columnTypes
+  })
 
   await browser.close()
 })()
+
+
